9628807369d54aeec11275780c017966b5e1c585
Frank Yang <yyf20001230@gmail.com>
2022-01-06 01:09:50 -0500

    firebase realtim database data storage

---
+++ src/utilities/firebase.js
@@ -0,0 +1,45 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref} from 'firebase/database';
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLjoRI5wwgwalznDb82fIzHYWVHpMElBE",
+    authDomain: "scheduler-d3ac7.firebaseapp.com",
+    projectId: "scheduler-d3ac7",
+    storageBucket: "scheduler-d3ac7.appspot.com",
+    messagingSenderId: "623904953442",
+    appId: "1:623904953442:web:f068a5a953815994bf6d3b",
+    measurementId: "G-D8TYWPVLW9"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+ref(database)
+ref(database, '/')
+ref(database, '/courses')
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+};
